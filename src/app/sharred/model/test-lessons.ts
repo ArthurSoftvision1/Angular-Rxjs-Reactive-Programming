6c4b8b918211a66008eb2c4b4f484f8a32bcab26
MolnarArthur <arthur.molnar@usource.ro>
2018-12-14 16:39:24 +0200

    Implementing the Observer pattern

---
+++ src/app/sharred/model/test-lessons.ts
@@ -0,0 +1,21 @@
+
+
+import { Lesson } from "./lesson";
+
+export const lessons: Lesson[] = [
+    {
+        id: 1,
+        description: 'Setting Up an Angular Development Environment',
+        duration: '5:00'
+    },
+    {
+        id: 2,
+        description: 'Running the Lessons Code',
+        duration: '7:00'
+    },
+    {
+        id: 3,
+        description: 'Build your First App - Hello World Step by Step',
+        duration: '9:00'
+    },
+]
