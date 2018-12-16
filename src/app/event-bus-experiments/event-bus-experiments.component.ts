7b26f9b2b7beb4423c4548ead766f75a13a0e614
MolnarArthur <arthur.molnar@usource.ro>
2018-12-16 10:57:31 +0200

    Add new lesson

--- src/app/event-bus-experiments/event-bus-experiments.component.ts
+++ src/app/event-bus-experiments/event-bus-experiments.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { globalEventBus, LESSONS_LIST_AVAILABLE } from './event-bus';
+import { globalEventBus, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON } from './event-bus';
 import { testLessons } from '../sharred/model/test-lessons';
 
 @Component({
@@ -13,6 +13,10 @@ export class EventBusExperimentsComponent implements OnInit {
 
   ngOnInit() {
     globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE, testLessons.slice(0))
+  }
+
+  addLesson(lessonText: string) {
+    globalEventBus.notifyObservers(ADD_NEW_LESSON, lessonText);
   }
 
 }
--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { globalEventBus, Observer, LESSONS_LIST_AVAILABLE } from '../event-bus-experiments/event-bus';
+import { globalEventBus, Observer, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON } from '../event-bus-experiments/event-bus';
 import { Lesson } from '../sharred/model/lesson';
 
 @Component({
@@ -13,11 +13,25 @@ export class LessonsListComponent implements Observer {
 
   constructor() { 
     globalEventBus.registerObserver(LESSONS_LIST_AVAILABLE, this);
+
+    globalEventBus.registerObserver(ADD_NEW_LESSON, {
+      notify: lessonText => {
+        this.lessons.push({
+          id: Math.random(),
+          description: lessonText
+        })
+      }
+    })
   }
 
   notify(data: Lesson[]) {
     console.log('Lessons list cmoponent')
     this.lessons = data;
+  }
+
+  toggleLessonViewed(lesson: Lesson) {
+    console.log('toggline lesson ...');
+    lesson.completed = !lesson.completed;
   }
 
 }
